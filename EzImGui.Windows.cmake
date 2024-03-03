d03fd76379ec663d3fa4b97b9cf861023cfbd623
Sheen Tian <sheentianshen@gmail.com>
2024-03-03 23:53:34 +0800

    switch to fetch content mode

--- EzImGui.Windows.cmake
+++ EzImGui.Windows.cmake
@@ -2,36 +2,36 @@ option(EZIG_ENABLE_GLFW "Build ezImGui as shared library" ON)
 
 # backend header files
 file(GLOB _backend_HEADER_FILES
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_glfw.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_opengl2.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_opengl3.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_win32.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_win32.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_dx9.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_dx10.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_dx11.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_dx12.h
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl2.h
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.h
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_win32.h
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_win32.h
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_dx9.h
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_dx10.h
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_dx11.h
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_dx12.h
 )
 
 # backend source file
 file(GLOB _backend_SOURCE_FILES
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_glfw.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_opengl2.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_opengl3.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_opengl3_loader.h
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_win32.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_dx9.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_dx10.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_dx11.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_dx12.cpp
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl2.cpp
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3_loader.h
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_win32.cpp
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_dx9.cpp
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_dx10.cpp
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_dx11.cpp
+  ${imgui_SOURCE_DIR}/backends/imgui_impl_dx12.cpp
 )
 
 if(EZIG_ENABLE_GLFW)
   add_library(glfw3 STATIC IMPORTED)
   set_target_properties(glfw3
     PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_CURRENT_SOURCE_DIR}/imgui/examples/libs/glfw/include
-    IMPORTED_LOCATION ${CMAKE_CURRENT_SOURCE_DIR}/imgui/examples/libs/glfw/lib-vc2010-$(PlatformArchitecture)/glfw3.lib
+    INTERFACE_INCLUDE_DIRECTORIES ${imgui_SOURCE_DIR}/examples/libs/glfw/include
+    IMPORTED_LOCATION ${imgui_SOURCE_DIR}/examples/libs/glfw/lib-vc2010-$(PlatformArchitecture)/glfw3.lib
   )
 endif()
 
@@ -55,12 +55,12 @@ endmacro(add_ezimgui_target)
 
 macro(add_exmaple_target _target)
   add_executable(${_target}
-    ${CMAKE_CURRENT_SOURCE_DIR}/imgui/examples/example_${_target}/main.cpp
+    ${imgui_SOURCE_DIR}/examples/example_${_target}/main.cpp
   )
   target_include_directories(${_target}
     PRIVATE
-    ${CMAKE_CURRENT_LIST_DIR}/imgui
-    ${CMAKE_CURRENT_LIST_DIR}/imgui/backends
+    ${imgui_SOURCE_DIR}
+    ${imgui_SOURCE_DIR}/backends
   )
   target_link_libraries(${_target}
     PRIVATE
